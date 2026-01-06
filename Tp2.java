b904a400f4b2051ec0c7256be37366a52107894d
Lucas ALLAIN <92527225+Lacamm@users.noreply.github.com>
2026-01-06 14:56:54 +0100

    Fibo recursif

--- Tp2.java
+++ Tp2.java
@@ -8,27 +8,29 @@ public class Tp2 {
         scanner = new Scanner(System.in);
 
         // fibo();
-        fiboTab();
+        // fiboTab();
+        fibo3();
 
         scanner.close();
     }
 
     public static void fibo() {
-        /* régles fibo
-        F(0) = 0
-        F(1) = 1
-        F(n) = F(n)-1 + F(n)-2
-         */ 
+        /*
+         * régles fibo
+         * F(0) = 0
+         * F(1) = 1
+         * F(n) = F(n)-1 + F(n)-2
+         */
         int nombre;
         int nb1 = 0;
         int nb2 = 1;
         int res = 0;
         System.out.print("Saisir un nombre : ");
-		nombre = scanner.nextInt();
+        nombre = scanner.nextInt();
 
-        for (int i=0;i<=nombre;i++) {
-            if (i>0){
-                if (i==1){
+        for (int i = 0; i <= nombre; i++) {
+            if (i > 0) {
+                if (i == 1) {
                     res = 1;
                 } else {
                     res = nb1 + nb2;
@@ -46,12 +48,12 @@ public class Tp2 {
         int nb1 = 0;
         int nb2 = 1;
         System.out.print("Saisir un nombre : ");
-		nombre = scanner.nextInt();
-        int[] res = new int[nombre+1];
+        nombre = scanner.nextInt();
+        int[] res = new int[nombre + 1];
 
-        for (int i=0;i<=nombre;i++) {
-            if (i>0){
-                if (i==1){
+        for (int i = 0; i <= nombre; i++) {
+            if (i > 0) {
+                if (i == 1) {
                     res[i] = 1;
                 } else {
                     res[i] = nb1 + nb2;
@@ -65,4 +67,20 @@ public class Tp2 {
         System.out.println(res[nombre]);
         System.out.println("Fibo à F" + nombre + " = " + res[nombre]);
     }
+
+    public static void fibo3(){
+        int nombre;
+        System.out.print("Saisir un nombre : ");
+        nombre = scanner.nextInt();
+        for (int i=2; i<=nombre; i++){
+            System.out.print(fiboRecu(i) + " ");
+        }
+    }
+
+    public static int fiboRecu(int val) {
+        if (val < 2){
+            return val;
+        }
+        return fiboRecu(val - 1) + fiboRecu(val - 2);
+        }
 }
